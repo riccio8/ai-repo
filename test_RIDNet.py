221a10eedd696126fee5aa204fbc164c0996d4a3
0x90 <134144784+riccio8@users.noreply.github.com>
2025-10-04 15:09:26 +0200

    Update test_RIDNet.py

--- test_RIDNet.py
+++ test_RIDNet.py
@@ -3,31 +3,40 @@ from PIL import Image
 from torchvision import transforms
 from train import RIDNet
 
-# Percorso modello addestrato
+
 model_path = "/content/ridnet_sidd.pth"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Carica modello
-model = RIDNet()  # Assicurati di avere la classe RIDNet importata
+model = RIDNet()  
 model.load_state_dict(torch.load(model_path, map_location=device))
 model.to(device)
 model.eval()
 
-# Trasformazioni per l'immagine (come durante training)
 transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
-# Carica una immagine di test
 img = Image.open("/content/noisy.jpg").convert("RGB")
-img_tensor = transform(img).unsqueeze(0).to(device)  # batch dimension
+img_tensor = transform(img).unsqueeze(0).to(device)  
 
-# Passa il modello
 with torch.no_grad():
     output = model(img_tensor)
 
-# output è un tensore PyTorch, lo puoi salvare come PNG
-output_img = output.squeeze(0).cpu().clamp(0,1)  # rimuove batch, limita valori
+output_img = output.squeeze(0).cpu().clamp(0,1) 
 output_pil = transforms.ToPILImage()(output_img)
 output_pil.save("denoised_test.tiff")
 print("Test completato, immagine salvata come denoised_test.png")
+
+
+# Create example inputs for exporting the model. The inputs should be a tuple of tensors.
+example_inputs = (torch.randn(1, 3, 128, 128),) # 1 batch, 3 canali non scala di ggrigi e 12*18 (anche altri valori)
+onnx_program = torch.onnx.export(model, example_inputs, dynamo=True)
+
+onnx_program.save("image_denoise.onnx")
+print("model onnx exported")
+
+import onnx
+
+onnx_model = onnx.load("image_denoise.onnx")
+onnx.checker.check_model(onnx_model)
